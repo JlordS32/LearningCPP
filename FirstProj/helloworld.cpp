8cfa159cb3a8e116d4cceb1f497e01fee6c38b90
JlordS32 <jlordsty128@gmail.com>
2024-06-24 12:16:29 +1000

    progress 1

---
+++ FirstProj/helloworld.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+int main() {
+
+    // Hello, World! This is my first C++ program.
+    std::cout << "Hello, World!" << std::endl;
+
+    // 1 + 1 = 2
+    std::cout << "1 + 1 = " << 1 + 1 << std::endl;
+
+    /*
+    
+    This is a multi-line comment
+
+    */
+
+    return 0;
+}
