495275d69ca2a23344dd1dce0add6ba23b54e92a
JlordS32 <jlordsty128@gmail.com>
2024-06-24 12:30:20 +1000

    finished variables

---
+++ FirstProj/variables.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string name = "Bro";
+    int number = 9;
+    double someDouble = 9.3;
+
+    cout << name << " is the best" << "\nmotherfucker" << endl;
+    cout << number + someDouble << endl;
+    cout << number / 2 << endl;
+
+    return 0;
+}
