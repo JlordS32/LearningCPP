a55805b8cb579de654cae5f4868127ae28de8d29
JlordS32 <jlordsty128@gmail.com>
2024-06-24 12:34:44 +1000

    finished const

---
+++ FirstProj/constants.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    const int PI = 3.14;
+    double radius = 5.6;
+    double circumference = 2 * PI * radius;
+
+    cout << "The circumference of a circle with radius " << radius << " is " << circumference << "cm." << endl;
+
+    return 0;
+}
