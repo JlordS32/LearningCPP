f81fd7ae84f43a91c111cff609e18b422f0f3246
JlordS32 <jlordsty128@gmail.com>
2024-06-25 17:11:47 +1000

    done

---
+++ FirstProj/fillUsingInput.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+int main() {
+
+    std::string foods[5];
+    std::string userInput;
+    int size = sizeof(foods)/sizeof(foods[0]);
+
+    for (int i = 0; i < size; i++) {
+        std::cout << "Enter a food #" << i + 1 << ": ";
+        std::getline(std::cin, userInput);
+
+        if (userInput == "q") {
+            break;
+        } else {
+            foods[i] = userInput;
+        }
+    }
+
+    std::cout << "\nYou like the following food: \n" << std::endl;
+
+    for (std::string food : foods) {
+        if (!food.empty()) {
+            std::cout << food << std::endl;
+        }
+    }
+
+    std::cout << std::endl;
+    
+    return 0;
+}
