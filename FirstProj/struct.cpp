5992bdc78c1c94a22e438301f84c73c5c77cb899
JlordS32 <jlordsty128@gmail.com>
2024-06-27 19:26:25 +1000

    done

---
+++ FirstProj/struct.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+struct student
+{
+    std::string name;
+    int age;
+    float gpa;
+};
+
+int main()
+{
+    student student1 = {"Bro", 21, 3.5};
+    student student2;
+
+    student2.name = "Nerd";
+    student2.age = 22;
+    student2.gpa = 4.0;
+
+    std::cout << student1.name << " " << student1.age << " " << student1.gpa << std::endl;
+    std::cout << student2.name << " " << student2.age << " " << student2.gpa << std::endl;    
+
+    return 0;
+}
