84e6395dc34d4c5a1f9fe65667e0e6e524be2857
JlordS32 <jlordsty128@gmail.com>
2024-07-01 16:06:40 +1000

    done

--- Pointers/functionPointers.cpp
+++ Pointers/functionPointers.cpp
@@ -1,13 +1,26 @@
 #include <iostream>
 using namespace std;
 
-int getNumber() {
-    return 5;
+// A function that takes a function pointer that returns a type double and takes two parameters of type double.
+double calculate(double x, double y, double (*funcPtr)(double, double)) {
+    return funcPtr(x, y);
 }
 
 int main()
 {   
-    cout << getNumber;
+    // Lambda functions
+    auto addition = [](double x, double y) { return x + y; };
+    auto substraction = [](double x, double y) { return x - y; };
+    auto multiplication = [](double x, double y) { return x * y; };
+    auto division = [](double x, double y) { return x / y; };
+
+    double x = 30.4454;
+    double y = 6.7;
+
+    cout << "Addition: " << calculate(x, y, addition) << endl;
+    cout << "Subtraction: " << calculate(x, y, substraction) << endl;
+    cout << "Multiplication: " << calculate(x, y, multiplication) << endl;
+    cout << "Division: " << calculate(x, y, division) << endl;
     
     return 0;
 }
