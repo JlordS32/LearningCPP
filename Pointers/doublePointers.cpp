ba76a2b72df4ab03a55e0baf15f8ab14bccf27b5
JlordS32 <jlordsty128@gmail.com>
2024-07-01 14:55:43 +1000

    progress

--- Pointers/doublePointers.cpp
+++ Pointers/doublePointers.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
 
-int main() {
+using namespace std;
+
+int main()
+{
+    int someArray[][2] = {{1, 2}, {3, 4}};
+
+    int **pArr = new int *[2];
+
+    pArr[0] = someArray[0];
+    pArr[1] = someArray[1];
+
+    cout << "First element of first sub-array: " << pArr[0] << endl;
+    cout << "Second element of second sub-array: " << pArr[1] << endl;
+
+    pArr[0][1] = 69;
+
+    cout << someArray[0][1] << endl;
 
     return 0;
 }
--- Pointers/tempCodeRunnerFile.cpp
+++ Pointers/tempCodeRunnerFile.cpp
@@ -1,1 +1,14 @@
-return 0;
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int someArray[][2] = {{1, 2}, {3, 4}};
+
+    int **pArr = new int *[2];
+
+    cout << pArr << endl;
+
+    return 0;
+}
